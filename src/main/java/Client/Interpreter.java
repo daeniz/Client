92148fbfd16cae71a782ffdb6d6f4447b1d9bbfd
daeniz <dennis@schmock.eu>
2016-09-08 10:17:39 +0200

    Handling ClientList

--- src/main/java/Client/Interpreter.java
+++ src/main/java/Client/Interpreter.java
@@ -1,6 +1,5 @@
 package Client;
 
-
 import java.io.IOException;
 import java.net.Socket;
 import java.util.Arrays;
@@ -18,24 +17,24 @@ import java.util.logging.Logger;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author Daniel
  */
-public class Interpreter extends Observable implements Runnable{
+public class Interpreter extends Observable implements Runnable {
+
     private Socket socket;
     private Scanner s;
     private AtomicBoolean loggedOut;
     private List<String> clientList;
-    
-    public Interpreter(Socket socket){
-        this.socket=socket;
-        loggedOut=new AtomicBoolean();
+
+    public Interpreter(Socket socket) {
+        this.socket = socket;
+        loggedOut = new AtomicBoolean();
         loggedOut.set(false);
         clientList = new CopyOnWriteArrayList<>();
         try {
-            s=new Scanner(socket.getInputStream());
+            s = new Scanner(socket.getInputStream());
         } catch (IOException ex) {
             Logger.getLogger(Interpreter.class.getName()).log(Level.SEVERE, null, ex);
         }
@@ -43,12 +42,11 @@ public class Interpreter extends Observable implements Runnable{
 
     @Override
     public void run() {
-        while (!loggedOut.get()){   //while we are not logged out..
-            String cmd="";
-            try{
-                cmd=s.nextLine();
-            }
-            catch (NoSuchElementException ex){
+        while (!loggedOut.get()) {   //while we are not logged out..
+            String cmd = "";
+            try {
+                cmd = s.nextLine();
+            } catch (NoSuchElementException ex) {
                 System.out.println("Socket vanished in front of our eyes!");
                 loggedOut.set(true);
             }
@@ -61,20 +59,25 @@ public class Interpreter extends Observable implements Runnable{
         }
         System.out.println("Exiting interpreter");
     }
-    
-    public void setLoggedOut(){
+
+    public void setLoggedOut() {
         loggedOut.set(true);
     }
-    
-    public void postMSG(String[] command){
+
+    public void postMSG(String[] command) {
         String sender = command[1];
-        System.out.println(sender+" spews: "+ command[2]+" out of his ***");
+        System.out.println(sender + " spews: " + command[2] + " out of his ***");
     }
 
     private void getClients(String[] split) {
-        clientList = new CopyOnWriteArrayList<String>(Arrays.asList(split));
+        String[] tmp;
+        if (split.length > 1) {
+            tmp = split[1].split(",");
+            clientList = new CopyOnWriteArrayList<String>(Arrays.asList(tmp));
+        }
         for (String string : clientList) {
             System.out.println(string);
         }
+
     }
 }
